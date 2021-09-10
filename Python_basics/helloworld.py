e1ac1b2c8eb58f1828c26c9f4c76c9dc587734bf
Mila Phlipsen <mila.philipsen@student.hku.nl>
2021-09-10 16:18:21 +0200

    How are you doing

--- Python_basics/helloworld.py
+++ Python_basics/helloworld.py
@@ -1,3 +1,9 @@
 print('type name')
-username = input ('...')
-print( 'hello ...', username)
+username = input ('')
+print( 'Hello', username + '!')
+print('How are you feeling today?')
+answer = input ('')
+if answer == 'good':
+    print('Good to hear.')
+if answer == 'could be better':
+    print('I am sure things will be okay')
