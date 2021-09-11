c08b30cf844c1d03c9138c84adfe6e69c7f5118f
Mila Phlipsen <mila.philipsen@student.hku.nl>
2021-09-11 17:10:31 +0200

    List of mood answers

--- Python_basics/helloworld.py
+++ Python_basics/helloworld.py
@@ -1,9 +1,17 @@
 print('type name')
 username = input ('')
 print( 'Hello', username + '!')
-print('How are you feeling today?')
-answer = input ('')
-if answer == 'good':
-    print('Good to hear.')
-if answer == 'could be better':
-    print('I am sure things will be okay')
+import inquirer #library die user interaction makkelijker maakt
+questions = [
+  inquirer.List('mood', #begin de lijst mood
+                message='How are you feeling today?',
+                choices=['Great', 'Could be better', 'Terrible'],
+            ),
+]
+answers = inquirer.prompt(questions) #het geselecteerde antwoord is answers
+if answers['mood'] == 'Great': #als het antwoord van de lijst mood, great is, dan...
+    print('Good to hear!')
+elif answers['mood'] == 'Could be better':
+    print('I am sure things will be alright.')
+else:
+    print(':(')
