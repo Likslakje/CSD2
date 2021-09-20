2da4e7ecf025735137f7cb22039605a02d368b49
Mila Phlipsen <mila.philipsen@student.hku.nl>
2021-09-20 23:41:06 +0200

    Sequencer with sleep() function

--- 2a/Python_basics/Rithmicplayback.py
+++ 2a/Python_basics/Rithmicplayback.py
@@ -1,13 +1,16 @@
+
+
+print("beetje later")
 print("What BPM should the sequence be?")
 bpmInput = input()
 bpm = int(bpmInput)
-bpmMilliseconds = 60000/bpm
+bpmSeconds = 60/bpm #kwartnoot in 4/4, 1 beat
 
 print("How many steps should the sequence have?")
 steps = input()
 numberSteps = int(steps) #maak een interger van de input
 
-print("Isert rythmic patern * use point for floats, not comma *")
+print("Insert rythmic patern * use point for floats, not comma *")
 durationStepsArray = [] #maak een lege array om het ritme in op te slaan
 for x in range(numberSteps): #een forloop zolang als het aantal steps
     durationStepsInput = input() #vraag nu voor elke element in de lijst (zolang als numberSteps) een input (duration)
@@ -17,7 +20,9 @@ for x in range(numberSteps): #een forloop zolang als het aantal steps
 
 import simpleaudio as sa
 wave_obj = sa.WaveObject.from_wave_file("../samples/Kick.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
-print (durationStepsArray)
-print(bpmMilliseconds)
+for x in range(numberSteps):
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+    import time
+    time.sleep(bpmSeconds * x)
+    x =+ 1
