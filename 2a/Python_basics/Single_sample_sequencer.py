29647d2beabc87b215b5f05ff83d9ae8e197e50c
Mila Phlipsen <mila.philipsen@student.hku.nl>
2021-09-25 22:51:49 +0200

    bpm user question

--- 2a/Python_basics/Single_sample_sequencer.py
+++ 2a/Python_basics/Single_sample_sequencer.py
@@ -1,19 +1,26 @@
 import simpleaudio as sa
 import time
 
-print ("wats die bpm a zebi")
-bpmInput = input()
-bpm = int(bpmInput)
+def choice(): #maak een functie voor de y/n vraag voor bpm
+    bpmChoice = input("Default BPM is set to 120, would you like to change the BPM (y/n): ")
+    if bpmChoice == "n" :
+        bpm = 120
+        return bpm
+    elif bpmChoice == "y" :
+        bpm = int(input("Enter a new BPM: " ))
+        return bpm
+    else :
+        print("Please enter valid input, 'y' or 'n' only")
+        choice() #als input niet matched dan voer de functie choice opnieuw uit
+bpm = choice() #bpm is gelijk aan het resultaat van de functie choice
 
 print("How many steps shoukd the sequence have?")
-numberNotesInput = input()
-numberNotes = int(numberNotesInput)
+numberNotes = int(input())
 
-print("choose " + numberNotesInput + " note durations")
+print("choose " + str(numberNotes) + " note durations")
 noteDurations = []
 for i in range(numberNotes):
-    noteDurationsInput = input()
-    noteDurations.append(float(noteDurationsInput))#voeg telkens de ingeven waarde toe aan de array
+    noteDurations.append(float(input()))#voeg telkens de ingeven waarde toe aan de array
 print(noteDurations)
 
 timeStamps16th = [] #een inital 0 voor de eerste step in de sequence
@@ -32,3 +39,6 @@ def sixteenthTimeStampsToSeconds (_timeStamps16th, _bpm): #_timeStamps16th (ande
         timeStamps.append(i * sixteenthnoetDuration)
 sixteenthTimeStampsToSeconds(timeStamps16th, bpm) #pass de lijst met timeStamps16th naar de functie sixteenthTimeStampsToSeconds
 print(timeStamps)
+
+timeZero = time.time()
+timeEalepsed = 0
