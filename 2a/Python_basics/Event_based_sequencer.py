617f2768b5f434679827b52bcfaf13e6362d4adb
Mila Phlipsen <mila.philipsen@student.hku.nl>
2021-10-08 16:29:28 +0200

    noteDurations -> noteDurations16th

--- 2a/Python_basics/Event_based_sequencer.py
+++ 2a/Python_basics/Event_based_sequencer.py
@@ -2,27 +2,50 @@ import simpleaudio as sa
 import time
 
 kick = sa.WaveObject.from_wave_file("../samples/Kick.wav")
+snare = sa.WaveObject.from_wave_file("../samples/Snare.wav")
+hihat = sa.WaveObject.from_wave_file("../samples/Hihat.wav")
 
 kick_event = {
-    'filename': Kick
-    'instrumentname': 'kick'
-    'timestamp': 1000
-    'duration': 100
-    'velocity': 127
+    'filename': kick,
+    'instrumentname': 'kick',
+    'timestamp': 1000,
+    'duration': 100,
+    'velocity': 127,
 }
 
 snare_event = {
-    'filename': Snare
-    'instrumentname': 'snare'
-    'timestamp': 2000
-    'duration': 200
-    'velocity': 127
+    'filename': snare,
+    'instrumentname': 'snare',
+    'timestamp': 2000,
+    'duration': 200,
+    'velocity': 127,
 }
 
 hihat_event = {
-    'filename': Hihat
-    'instrumentname': 'hihat'
-    'timestamp': 3000
-    'duration': 300
-    'velocity': 127
+    'filename': hihat,
+    'instrumentname': 'hihat',
+    'timestamp': 3000,
+    'duration': 300,
+    'velocity': 127,
 }
+
+bpm = 120
+noteDurations = [0.5, 1, 2, 1, 2]
+
+noteDuration16th = []
+def make_note_durations_16th(_noteDurations):
+    i = 0
+    for i in range(len(noteDurations)):
+        noteDuration16th.append(float(_noteDurations[i]) / 0.25)
+        i += 1
+make_note_durations_16th(noteDurations)
+
+print(noteDuration16th)
+
+# def handle_sample_event(event):
+#     print(event['instrumentname'])
+#     event['filename'].play()
+# handle_sample_event(kick_event)
+# time.sleep(1)
+# handle_sample_event(snare_event)
+# time.sleep(1)
