f07462954dd4e82c0aa18d7c68fb77108fa13399
Mila Phlipsen <mila.philipsen@student.hku.nl>
2021-10-10 14:57:49 +0200

    allSampleDict zodat rythmic input in de juiste sample dict komt

--- 2a/Python_basics/Event_based_sequencer.py
+++ 2a/Python_basics/Event_based_sequencer.py
@@ -30,6 +30,8 @@ hihat_event = {
     'velocity': 127,
 }
 
+allSampleDict = {'kick' : kick_event, 'snare' : snare_event, 'hihat' : hihat_event} # dit is een nested dictionary
+
 bpm = 120
 noteDurations = [0.5, 1, 2, 1, 2]
 
@@ -48,7 +50,6 @@ noteDurations = [0.5, 1, 2, 1, 2]
 #         bpmchoice() #als input niet matched dan voer de functie choice opnieuw uit
 # bpm = bpmchoice() #bpm is gelijk aan het resultaat van de functie choice
 
-noteDurationsForInstrument = {}
 def instrumentname_noteDuratons_choice():
     # aan de hand de gekozen instrumentname en het aantal steps worden er notedurations aan de array toegevoegd
 
@@ -64,48 +65,44 @@ def instrumentname_noteDuratons_choice():
     print("Choose number of steps for " + str(instrumentChoiceAnswer['instrumentname']) + " drumline")
     numberSteps = int(input())
 
-    # een dictionary betsaande uit de instrumentname met de bij behorende noteDurations array
-    global noteDurationsForInstrument # maak de dictionary binnen de fuctie beschikbaar buiten de functie
-    noteDurationsForInstrument = {
-        'instrumentname' : instrumentChoiceAnswer['instrumentname'],
-        'noteDurations' : []
-    }
     print("Type rythmic patern for " + str(instrumentChoiceAnswer['instrumentname']) + " drumline")
-    for i in range(numberSteps):
-        noteDurationsForInstrument['noteDurations'].append(float(input()))
-        i += 1
+    # een dictionary betsaande uit de instrumentname met de bij behorende noteDurations array
+    if instrumentChoiceAnswer['instrumentname'] in allSampleDict :
+        for i in range(numberSteps):
+            # voeg zoveel elementen (als er numberSteps zijn) aan de timestamps array, van kick_event binnen allSampleDict, toe 
+            allSampleDict[instrumentChoiceAnswer['instrumentname']]['timestamps'].append(float(input()))
+            i += 1
 instrumentname_noteDuratons_choice()
-print(noteDurationsForInstrument['noteDurations'])
 
 
-def duration_16th_note(_bpm):
-    # de lengte van 1 16de noot wordt uitgereknd aan de hadn van het bpm
+# def duration_16th_note(_bpm):
+#     # de lengte van 1 16de noot wordt uitgereknd aan de hadn van het bpm
 
-    return((60 / _bpm) / 4)# hele noot = 60/bpm, 16de = 1/4 van een hele noot
-noteDuration16th = duration_16th_note(bpm)
+#     return((60 / _bpm) / 4)# hele noot = 60/bpm, 16de = 1/4 van een hele noot
+# noteDuration16th = duration_16th_note(bpm)
 
-noteDurationArray16th = []
-def noteDurations_to_noteDurations16th(_noteDurations):
-    # zet voor het aantal elementen in noteDurations elk element om in een 16de
-    i = 0
-    for i in range(len(noteDurationsForInstrument['noteDurations'])):
-        noteDurationArray16th.append(float(_noteDurations[i]) / 0.25)
-        i += 1
-noteDurations_to_noteDurations16th(noteDurationsForInstrument['noteDurations'])
-print(noteDurationArray16th)
+# noteDurationArray16th = []
+# def noteDurations_to_noteDurations16th(_noteDurations):
+#     # zet voor het aantal elementen in noteDurations elk element om in een 16de
+#     i = 0
+#     for i in range(len(noteDurationsForInstrument['noteDurations'])):
+#         noteDurationArray16th.append(float(_noteDurations[i]) / 0.25)
+#         i += 1
+# noteDurations_to_noteDurations16th(noteDurationsForInstrument['noteDurations'])
+# print(noteDurationArray16th)
 
-timeStamps = []
-def noteDurations16th_to_timeStamps(_noteDurationArray16th):
-    # zet elke 16de noot duratie om in een tijd in miliseconden daarna tellen deze tijden op
-    timeStamps16th = []
-    sum = 0
-    for i in range(len(noteDurationsForInstrument['noteDurations'])):
-        timeStamps16th.append(float(_noteDurationArray16th[i] * noteDuration16th))
-        sum = sum + timeStamps16th[i]
-        timeStamps.append(sum)
-        print(sum)
-noteDurations16th_to_timeStamps(noteDurationArray16th)
-print(timeStamps)
+# timeStamps = []
+# def noteDurations16th_to_timeStamps(_noteDurationArray16th):
+#     # zet elke 16de noot duratie om in een tijd in miliseconden daarna tellen deze tijden op
+#     timeStamps16th = []
+#     sum = 0
+#     for i in range(len(noteDurationsForInstrument['noteDurations'])):
+#         timeStamps16th.append(float(_noteDurationArray16th[i] * noteDuration16th))
+#         sum = sum + timeStamps16th[i]
+#         timeStamps.append(sum)
+#         print(sum)
+# noteDurations16th_to_timeStamps(noteDurationArray16th)
+# print(timeStamps)
 
 # def handle_sample_event(event):
 #     print(event['instrumentname'])
