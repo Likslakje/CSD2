1542c0cb841e47af9faa43b876a62b19ed04fd48
Mila Phlipsen <mila.philipsen@student.hku.nl>
2021-11-20 03:09:58 +0100

    Add kill_bill folder

---
+++ 2b/cpp/kill_bill/kill_bill.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+class UmaTherman{
+    int name;
+    int enemies; //list
+    void goal();   // als enemies bill is dan kill bill
+
+};
+
+class fightingSkills{
+    void punch(); // function om uit te voeren op enemies in for loop
+    void kick();
+    void katana();
+};
+
+class Vehecile{
+    int pussywagon;
+    int plane;
+    int motorcycle;
+    std::string color;
+
+};
+
+class Outfits{
+    int hospital;
+    int dress;
+    int motorcycleSuit;
+    
+};
+
+void punch(int enemies){
+    std::cout<< 'killed' + enemy + 'by punching' <<std::endl;
+}
+
+void kick(int enemies){
+    std::cout<< 'killed' + enemy + 'by kicking' <<std::endl;
+};
+
+void katana(int enemies){
+    std::cout<< 'killed' + enemy + 'in katana duel' <<std::endl;
+};
+// 
