d7f911664159a0933c25b8a7087c215c246fff1b
Mila Phlipsen <mila.philipsen@student.hku.nl>
2021-11-20 04:02:06 +0100

    Make your own animal

--- 2b/cpp/cat_cow/cat_cow.cpp
+++ 2b/cpp/cat_cow/cat_cow.cpp
@@ -17,15 +17,15 @@ class Animal{
         int numberLegs;
         int numberEyes;
         int numberEars;
-        void print();
-        void typeEyes(); // lefttype and righttype (numbereyes)
-        void typeEars(); // lefttype and righttype (numberears)
-        void typeNose();
-        void typeTail();
-        void typeTeeth();
-        void typeFeet();
+        void wholeAnimal();
+        void eyes(int numberEyes, string typeEyes); // lefttype and righttype (numbereyes)
+        void ears(int numberEars, string typeEars); // lefttype and righttype (numberears)
+        void nose(string typeNose);
+        void tail(string typeTail);
+        void teeth(string typeTeeth);
+        void feet(string typeFeet);
         void furColor();
-        void numberLegs();
+        void legs(int numberLegs);
 };
 
 Animal::Animal(string typeEyes, string typeEars, string typeNose, string typeTail, string typeTeeth, string typeFeet, int numberLegs, int numberEyes, int numberEars){
@@ -45,40 +45,63 @@ Animal::~Animal(){
 
 }
 
-void Animal::print(){
-    cout<< typeFeet <<endl;
-    cout<< typeEars <<endl;
+void Animal::wholeAnimal(){
+    cout<< typeEyes + " "
+        << typeEars + " "
+        << typeNose + " "
+        << typeTail + " "
+        << typeTeeth + " "
+        << typeFeet + " "
+        << numberLegs
+        << " "
+        << numberEyes
+        << " "
+        << numberEars <<endl;
 }
 
-void typeEyes(){
-    // if (numberEyes =< 2){
-    //     cout<< ty
-    // }
+void Animal::eyes(int numberEyes, string typeEyes){
+    if (numberEyes <= 2){
+        cout<< typeEyes <<endl;
+    }else{
+        cout<< "Do you want to make a monster?" <<endl;
+    }
 }
 
-void typeEars(){
-
+void Animal::ears(int numberEars, string typeEars){
+      if (numberEars <= 2){
+        cout<< typeEars <<endl;
+    }else{
+        cout<< "Do you want to make a monster?" <<endl;
+    }
 }
 
-void typeNose(){
-
+void Animal::nose(string typeNose){
+    cout<< typeNose <<endl;
 }
 
-void typeTail(){
-
+void Animal::tail(string typeTail){
+    cout<< typeTail <<endl;
 }
 
-void typeTeeth(){
-
+void Animal::teeth(string typeTeeth){
+    cout<< typeTeeth <<endl;
 }
 
 int main(){
     // string typeEyes, string typeEars, string typeNose, 
     // string typeTail, string typeTeeth, int numberPaws, 
     // int numberLegs, int numberEyes, int numberEars
-    Animal cat("green", "pointy", "small", "fluffy", "sharp", "paws", 4, 2, 2);
+    Animal cat("green", "pointy", "small", "fluffy", "sharp", "paws", 4, 2, 2); //default
+    cat.wholeAnimal(); // print whole animal
+
     Animal cow("brown", "round", "big", "brush", "flat", "hooves", 4, 2, 2);
-    cat.print();
-    cow.print();
+    cow.wholeAnimal();
+
+    Animal catCow("none","none","none","none","none","none", 0, 0, 0);
+    catCow.eyes(1, "red");
+    catCow.ears(2, "round");
+    catCow.nose("small");
+    catCow.tail("fluffy");
+    catCow.teeth("vampire");
 
 }
