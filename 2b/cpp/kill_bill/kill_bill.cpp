b1f61b9adc9f4d6782b523c4333a064f72edb789
Mila Phlipsen <mila.philipsen@student.hku.nl>
2021-11-23 10:20:37 +0100

    Added arrays

--- 2b/cpp/kill_bill/kill_bill.cpp
+++ 2b/cpp/kill_bill/kill_bill.cpp
@@ -1,42 +1,74 @@
 #include <iostream>
+using namespace std; 
 
-class UmaTherman{
-    int name;
-    int enemies; //list
-    void goal();   // als enemies bill is dan kill bill
+string deadlyViperAssassinationSquad[6] = {"BlackMamba", "Cottonmouth", "Copperhead", "Sidewinder", "CalaforniaMountainSnake", "Bill"};
+string characters[4] = {"Buck", "SophieFatale", "Crazy88", "GogoYubari"};
+string weapons[5] = {"gun", "knife", "door", "katana", "flail"};
+string outfits[6] = {"hospitalGown", "weddingDress", "jeans", "motorcycleSuit", "suit", "kimono"};
 
+class Person{
+    public:
+        Person(string name, string weapon, string outfit);
+        ~Person();
+        string name;
+        string weapon;
+        string outfit;
+        void print();
 };
 
-class fightingSkills{
-    void punch(); // function om uit te voeren op enemies in for loop
-    void kick();
-    void katana();
-};
-
-class Vehecile{
-    int pussywagon;
-    int plane;
-    int motorcycle;
-    std::string color;
-
-};
-
-class Outfits{
-    int hospital;
-    int dress;
-    int motorcycleSuit;
-    
-};
-
-void punch(int enemies){
-    std::cout<< 'killed' + enemy + 'by punching' <<std::endl;
+Person::Person(string name, string weapon, string outfit){
+    this-> name = name;
+    this-> weapon = weapon;
+    this-> outfit = outfit;
 }
 
-void kick(int enemies){
-    std::cout<< 'killed' + enemy + 'by kicking' <<std::endl;
+Person::~Person(){}
+
+class Vol1{
+    //do the things in Vol.1
+    public:
+        Vol1();
+        ~Vol1();
 };
 
-void katana(int enemies){
-    std::cout<< 'killed' + enemy + 'in katana duel' <<std::endl;
+Vol1::Vol1(){
+
+}
+
+Vol1::~Vol1(){}
+
+class Vol2{
+    //do the tings in Vol.2
+    public:
+        Vol2();
+        ~Vol2();
+        void print();
 };
-// 
+
+Vol2::Vol2(){
+
+}
+
+Vol2::~Vol2(){}
+
+void Person::print(){
+    cout<< name << weapon << outfit <<endl;
+}
+
+int main(){
+    /*
+    Bride gets shot by Bill weddingdress/jeans
+    Bride kills Copperhead with knife jeans/jeans
+    Bride kills Buck with door HospitalGown/jeans
+    Bride amputates Sophie Fatale with katana motorcyclesuit/kimono
+    Bride kills the whole Crazy 88 with katana/katana motorcyclesuit/suit
+    Bride kills Gogo katana/flail
+    Bride kills Cottonmouth Katana/katana motorcyclesuit/kimono
+    */
+
+    Person Bride(deadlyViperAssassinationSquad[0], weapons[3], outfits[3]);
+    Bride.print();
+
+    Person VernitaGreen(deadlyViperAssassinationSquad[2], weapons[1], outfits[2]);
+    VernitaGreen.print();
+}
