9957ecf195739a8a7fb5752ef3861507c35b555e
Mila Philipsen <mila.philipsen@student.hku.nl>
2021-11-29 23:42:33 +0100

    Added cin test folder

---
+++ 2b/cpp/tests/cin/cin.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string instrument, sound, note;
+    cin>> instrument >> sound >> note;
+    cout<< instrument << sound << note <<endl;
+    return 0;
+}
