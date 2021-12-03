0c83d9e3678a75cd1235017e71746022e0dfdebe
Mila Philipsen <mila.philipsen@student.hku.nl>
2021-12-03 17:17:33 +0100

    Input works half

--- 2b/cpp/eindopdracht/input.cpp
+++ 2b/cpp/eindopdracht/input.cpp
@@ -3,31 +3,8 @@ using namespace std;
 #include "input.h"
 
 UserInput::UserInput(){
+//    waveformChoiceCheck();
     waveformChoice();
-    while (true) {
-        for(int i = 0; i < 4; i++){
-            try{
-                if(inputWaveform != waveforms[i]){
-                    if(i == 3){
-                        // I think something's going wrong here the second time around.
-                        // is it even allowed to have a while loop inside a class?
-                        cout<< inputWaveform << " does not exist" <<endl;
-                        waveformChoice(); // code too slow because of everything that needs to be printed?
-                        i = 0;
-                        // would continue; help ?
-                    }
-                }else{
-                    throw(inputWaveform);
-                }
-            }
-            catch(string waveformCatch){
-                i = 0;
-                cout<< inputWaveform << " exists" <<endl;
-                break;
-            }
-        }
-        break;
-    }
 }
 
 UserInput::~UserInput(){
@@ -36,9 +13,60 @@ UserInput::~UserInput(){
 
 void UserInput::waveformChoice(){
     cout<< "Choose waveform: ";
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < waveforms.size(); i++){
         cout<< waveforms[i] << " ";
     }
     cout<< endl;
     cin>> inputWaveform;
+
+            for(int i = 0; i < waveforms.size(); i++){
+                if(inputWaveform == waveforms[i]){
+                    cout<< "bool voor if " << choiceCheck << "i voor if "<< i <<endl;
+                    choiceCheck = true;
+                    cout<< "bool " << choiceCheck << "i " << i <<endl;
+                    i = 0;
+                    break;
+                }else{
+                    if(i > waveforms.size()){
+                        cout << "doesnt exist" <<endl;
+                    }else{
+                        cout<< "else" << i <<endl;
+                        // i++;
+                    }
+                    
+                }
+            }
+
+}
+
+void UserInput::waveformChoiceCheck(){
+// do{
+//     waveformChoice();
+// }
+// while(choiceCheck)
+//     waveformChoice();
+//     while (true) {
+//         for(int i = 0; i < 4; i++){
+//             try{
+//                 if(inputWaveform != waveforms[i]){
+//                     if(i == 3){
+//                         // I think something's going wrong here the second time around.
+//                         // is it even allowed to have a while loop inside a class?
+//                         cout<< inputWaveform << " does not exist" <<endl;
+//                         waveformChoice(); // code too slow because of everything that needs to be printed?
+//                         i = 0;
+//                         // would continue; help ?
+//                     }
+//                 }else{
+//                     throw(inputWaveform);
+//                 }
+//             }
+//             catch(string waveformCatch){
+//                 i = 0;
+//                 cout<< inputWaveform << " exists" <<endl;
+//                 break;
+//             }
+//         }
+//         break;
+//     }
 }
--- 2b/cpp/eindopdracht/input.h
+++ 2b/cpp/eindopdracht/input.h
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
+#include <array>
 
 class UserInput{
     public:
     UserInput();
     ~UserInput();
+    bool choiceCheck = false;
     void waveformChoice();
+    void waveformChoiceCheck();
     protected:
         string inputWaveform;  
-        string waveforms[4] = {"sine", "triangle", "saw", "square"};
+        array<string, 4> waveforms = {"sine", "triangle", "saw", "square"};
 };
