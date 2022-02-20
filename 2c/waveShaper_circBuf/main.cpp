39e8b819d699eab57ac0990dd1337981f976dc8e
Mila Phlipsen <mila.philipsen@student.hku.nl>
2022-02-20 01:46:00 +0100

    WOLLAH A KIFESH, its working. Implemented jack module, plot looks good. Fix: everything must be more efficient

--- 2c/waveShaper_circBuf/main.cpp
+++ 2c/waveShaper_circBuf/main.cpp
@@ -20,68 +20,67 @@ int main(int argc,char **argv)
   float amplitude = 0.5;
   Wavetable wavetable(samplerate, 8, Wavetable::WaveformType::SINE, 440);
 
- for(int i = 0; i < 200; i++){
-  //  wavetable.oscToWavetable();
-   std::cout<< "wavetable.getSampWavetable(): " << wavetable.getSampWavetable() <<std::endl;
- }
+//  for(int i = 0; i < 200; i++){
+//   //  wavetable.oscToWavetable();
+//    std::cout<< "wavetable.getSampWavetable(): " << wavetable.getSampWavetable() <<std::endl;
+//  }
  
 
 
-// #if WRITE_TO_FILE
-//   WriteToFile fileWriter("output.csv", true);
-//   // assign a function to the JackModule::onProces
-//   jack.onProcess = [&amplitude, &sine, &wavetable, &fileWriter](jack_default_audio_sample_t* inBuf,
-//     jack_default_audio_sample_t* outBuf, jack_nframes_t nframes) {
-// #else
-//   // assign a function to the JackModule::onProces
-//   jack.onProcess = [&amplitude, &sine, &wavetable](jack_default_audio_sample_t* inBuf,
-//     jack_default_audio_sample_t* outBuf, jack_nframes_t nframes) {
-// #endif
-//     for(unsigned int i = 0; i < nframes; i++) {
-//       wavetable.fillWavetable(sine.genNextSample());
-//       outBuf[i] = wavetable.getWavetableAtIndex();
-//       // ----- write result to file -----
-// #if WRITE_TO_FILE
-//       static int count = 0;
-//       if(count < WRITE_NUM_SAMPLES) {
-//         fileWriter.write(std::to_string(outBuf[i]) + "\n");
-//       } else {
-//         // log 'Done' message to console, only once
-//         static bool loggedDone = false;
-//         if(!loggedDone) {
-//           std::cout << "\n**** DONE **** \n"
-//             << "Output is written to file.\n"
-//             << "Please enter 'q' to quit the program." << std::endl;
-//           loggedDone = true;
-//         }
-//       }
-//       count++;
-//       // set output to 0 to prevent issues with output
-//       outBuf[i] = 0;
-// #endif
-//     }
+#if WRITE_TO_FILE
+  WriteToFile fileWriter("output.csv", true);
+  // assign a function to the JackModule::onProces
+  jack.onProcess = [&amplitude, &wavetable, &fileWriter](jack_default_audio_sample_t* inBuf,
+    jack_default_audio_sample_t* outBuf, jack_nframes_t nframes) {
+#else
+  // assign a function to the JackModule::onProces
+  jack.onProcess = [&amplitude, &wavetable](jack_default_audio_sample_t* inBuf,
+    jack_default_audio_sample_t* outBuf, jack_nframes_t nframes) {
+#endif
+    for(unsigned int i = 0; i < nframes; i++) {
+      outBuf[i] = wavetable.getSampWavetable();
+      // ----- write result to file -----
+#if WRITE_TO_FILE
+      static int count = 0;
+      if(count < WRITE_NUM_SAMPLES) {
+        fileWriter.write(std::to_string(outBuf[i]) + "\n");
+      } else {
+        // log 'Done' message to console, only once
+        static bool loggedDone = false;
+        if(!loggedDone) {
+          std::cout << "\n**** DONE **** \n"
+            << "Output is written to file.\n"
+            << "Please enter 'q' to quit the program." << std::endl;
+          loggedDone = true;
+        }
+      }
+      count++;
+      // set output to 0 to prevent issues with output
+      outBuf[i] = 0;
+#endif
+    }
 
-//     return 0;
-//   };
+    return 0;
+  };
 
-//   jack.autoConnect();
+  jack.autoConnect();
 
-//   //keep the program running and listen for user input, q = quit
-//   std::cout << "\n\nPress 'q' when you want to quit the program.\n";
-//   // boolean is used to keep program running / turn it off
-//   bool running = true;
-//   while (running)
-//   {
-//     switch (std::cin.get())
-//     {
-//       case 'q':
-//         running = false;
-//         jack.end();
-//         break;
-//       }
-//   }
+  //keep the program running and listen for user input, q = quit
+  std::cout << "\n\nPress 'q' when you want to quit the program.\n";
+  // boolean is used to keep program running / turn it off
+  bool running = true;
+  while (running)
+  {
+    switch (std::cin.get())
+    {
+      case 'q':
+        running = false;
+        jack.end();
+        break;
+      }
+  }
 
-//   //end the program
-//   return 0;
+  //end the program
+  return 0;
 
 } // main()
